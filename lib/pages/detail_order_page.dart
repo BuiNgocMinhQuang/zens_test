0fa5c5617821a2c78e741741fb42028a159e55ad
Bui Ngoc Minh Quang <buingocminhquang@gmail.com>
2024-02-06 19:45:22 +0700

    set_up

---
+++ lib/pages/detail_order_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DetailOrderPage extends StatelessWidget {
+  const DetailOrderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("DetailOrderPage"),
+    );
+  }
+}
